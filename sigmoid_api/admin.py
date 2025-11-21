5e29941ebd05c45b4ad670539e9cca9d1a492a22
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-11-20 21:49:56 -0600

    Modificaciones

--- sigmoid_api/admin.py
+++ sigmoid_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import SigmoidParameters
 
-# Register your models here.
+@admin.register(SigmoidParameters)
+class SigmoidParametersAdmin(admin.ModelAdmin):
+    list_display = ['id', 'x_shift', 'steepness', 'created_at']
+    list_filter = ['created_at']
